3d9b6b523000387383cd6bad43fc7e4e3c17bd8e
cgrama85@gmail.com <syntel@PHX440G3-28160V.syntelorg.com>
2018-01-11 00:04:16 -0700

    Adds a JSON library
    Proper data structure needed for the JSON object

--- OrdersDataAccess.java
+++ OrdersDataAccess.java
@@ -2,24 +2,29 @@ package MummysRestaurant;
 
 import java.sql.*;
 import java.util.*;
+import org.json.JSONObject;
 
 public class OrdersDataAccess {
     
-    static ResultSet getAllRows() throws SQLException{
-        
+    static JSONObject getAllRows() throws SQLException{
         try {
+            //query to be used to retrieve all rows from Orders
             String sqlQuery = "select * from orders";
             
+            //JSon object that will be returned
+            JSONObject jsonAllRows = new JSONObject();
+            
+            //established connectioned to oracle account through a driver
             Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","hr","hr");
             
-            Statement statement = conn.createStatement();
+            //executes the SQL query and returns a ResultSet object
+            ResultSet queryResults = conn.createStatement().executeQuery(sqlQuery);
             
-            ResultSet queryResults = statement.executeQuery(sqlQuery);
-            
-            return queryResults;
-            
+            while(queryResults.next()) {
+                //todo: convert ResultSet to JSon object
+            }
+            return jsonAllRows;
         } catch (SQLException ex) {
-            
             System.out.println(ex);
         }
         return null;
