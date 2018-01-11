e2ffb348af0e78a1535189720797b54d8446c773
cgrama85@gmail.com <syntel@PHX440G3-28160V.syntelorg.com>
2018-01-10 23:16:34 -0700

    Adds methods that queries the OrdersDB and returns all rows
    Right now it returns an object of type ResultSets, hoping
    to convert it into a JSon object.

--- OrdersDataAccess.java
+++ OrdersDataAccess.java
@@ -1,6 +1,27 @@
 package MummysRestaurant;
 
+import java.sql.*;
+import java.util.*;
 
 public class OrdersDataAccess {
     
+    static ResultSet getAllRows() throws SQLException{
+        
+        try {
+            String sqlQuery = "select * from orders";
+            
+            Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","hr","hr");
+            
+            Statement statement = conn.createStatement();
+            
+            ResultSet queryResults = statement.executeQuery(sqlQuery);
+            
+            return queryResults;
+            
+        } catch (SQLException ex) {
+            
+            System.out.println(ex);
+        }
+        return null;
+    }
 }
