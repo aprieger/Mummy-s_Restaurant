0489e5c903e5d50be575a7aa2727e66c7080377e
cgrama85@gmail.com <syntel@PHX440G3-28160V.syntelorg.com>
2018-01-11 21:45:47 -0700

    Adds OrdersPOJO class and incomplete method

--- OrdersDataAccess.java
+++ OrdersDataAccess.java
@@ -6,27 +6,65 @@ import org.json.JSONObject;
 
 public class OrdersDataAccess {
     
-    static JSONObject getAllRows() throws SQLException{
+    private JSONObject getAllOrdersForToday(String presentDate) throws SQLException{
         try {
-            //query to be used to retrieve all rows from Orders
-            String sqlQuery = "select * from orders";
-            
-            //JSon object that will be returned
             JSONObject jsonAllRows = new JSONObject();
-            
+            //query to be used to retrieve all rows from Orders
+            String sqlQuery = "select * from orders where DELIVERY_DATE = " + presentDate;
             //established connectioned to oracle account through a driver
             Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","hr","hr");
-            
+            //established a statement connection
+            Statement statement = conn.createStatement();
             //executes the SQL query and returns a ResultSet object
-            ResultSet queryResults = conn.createStatement().executeQuery(sqlQuery);
+            ResultSet queryResults = statement.executeQuery(sqlQuery);
             
             while(queryResults.next()) {
                 //todo: convert ResultSet to JSon object
             }
+            
+            statement.close();
+            conn.close();
+            
+            return jsonAllRows;
+            
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return null;
+    }
+    
+    private void deleteOrder(int order_id){
+        
+    }
+    
+    
+    
+    private JSONObject getAllRowsForCustomer(int customer_id) throws SQLException{
+        try {
+            //JSon object that will be returned
+            JSONObject jsonAllRows = new JSONObject();
+            //query to be used to retrieve all rows from Orders
+            String sqlQuery = "select * from orders where CUSTOMER_ID = " + Integer.toString(customer_id);
+            //established connectioned to oracle account through a driver
+            Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","hr","hr");
+            //established a statement connection
+            Statement statement = conn.createStatement();
+            //executes the SQL query and returns a ResultSet object
+            ResultSet queryResults = statement.executeQuery(sqlQuery);
+            
+            while(queryResults.next()) {
+                //todo: convert ResultSet to JSon object
+            }
+            
+            statement.close();
+            conn.close();
+            
             return jsonAllRows;
         } catch (SQLException ex) {
             System.out.println(ex);
         }
         return null;
     }
+    
+     
 }
